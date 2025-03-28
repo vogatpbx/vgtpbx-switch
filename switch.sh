32a9246154d0d69d5ee378660d30cdf36acf3862
kayp514 <pa.ka12@outlook.com>
2025-03-27 20:13:32 -0400

    Add PostgreSQL configuration support in switch.sh and update environment variables in docker-compose.yml
    This commit introduces a new environment variable, FREESWITCH_CORE_IN_POSTGRES, to enable PostgreSQL configuration for FreeSWITCH. The switch.sh script is updated to modify FreeSWITCH configuration files based on this variable, ensuring proper integration with PostgreSQL. Additionally, a minor formatting change is made in Dockerfile.small for better organization.

--- switch.sh
+++ switch.sh
@@ -2,6 +2,22 @@
 set -e  # Exit on error
 set -x  # Print commands for debugging
 
+# Configure FreeSWITCH based on FREESWITCH_CORE_IN_POSTGRES
+if [ "$FREESWITCH_CORE_IN_POSTGRES" = "yes" ]; then
+    echo "Updating FreeSWITCH configurations for PostgreSQL..."
+    # Update switch.conf.xml
+    sed -i "s|<!-- <param name=\"core-db-dsn\" value=\"\$\$\{dsn\}\" \/>\s*-->|<param name=\"core-db-dsn\" value=\"pgsql://hostaddr=$POSTGRES_HOST dbname=$SWITCH_DB_NAME user=$SWITCH_DB_USER password='$SWITCH_DB_PASSWORD'\" />|g" /etc/vgtpbx/freeswitch/autoload_configs/switch.conf.xml
+    sed -i 's|<!-- <param name="auto-create-schemas" value="false"\/>\s*-->|<param name="auto-create-schemas" value="true"/>|g' /etc/vgtpbx/freeswitch/autoload_configs/switch.conf.xml
+    sed -i 's|<!-- <param name="auto-clear-sql" value="false"\/>\s*-->|<param name="auto-clear-sql" value="true"/>|g' /etc/vgtpbx/freeswitch/autoload_configs/switch.conf.xml
+    # Update voicemail.conf.xml
+    sed -r -i 's/<!--(<param name="odbc-dsn" value="\$\$\{dsn\}"\/>)-->/\1/g' /etc/vgtpbx/freeswitch/autoload_configs/voicemail.conf.xml
+    sed -r -i 's/(<param name="dbname" value="\/var\/lib\/freeswitch\/vm_db\/voicemail_default.db"\/>)/<!--\1-->/g' /etc/vgtpbx/freeswitch/autoload_configs/voicemail.conf.xml
+    # Update fifo.conf.xml
+    sed -r -i 's/<!--(<param name="odbc-dsn" value="\$\$\{dsn\}"\/>)-->/\1/g' /etc/vgtpbx/freeswitch/autoload_configs/fifo.conf.xml
+    # Update db.conf.xml
+    sed -r -i 's/<!--(<param name="odbc-dsn" value="\$\$\{dsn\}"\/>)-->/\1/g' /etc/vgtpbx/freeswitch/autoload_configs/db.conf.xml
+fi
+
 # Wait for PostgreSQL
 until PGPASSWORD=$SWITCH_DB_PASSWORD psql -h $POSTGRES_HOST -U $SWITCH_DB_USER -d $SWITCH_DB_NAME -c '\q' 2>/dev/null; do
     echo "Waiting for PostgreSQL to be ready..."
